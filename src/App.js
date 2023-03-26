44ff2b4ec1de52b3736aabcc0acdcc9452b42054
konan080211 <konanforbis@gmail.com>
2023-03-26 11:52:49 +0900

    fix: タスクの追加

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Box from '@mui/material/Box';
 import { Container, Draggable } from "react-smooth-dnd";
 import {arrayMoveImmutable} from 'array-move';
@@ -52,25 +52,48 @@ export const App = () => {
   };
   */
 
-  const [items, setItems] = useState([
-    { id: "1", name: "初期1行目", est: 5, passed: 2, order: 0 },
-    { id: "2", name: "初期2行目", est: 5, passed: 2, order: 1 },
-    { id: "3", name: "初期3行目", est: 10, passed: 2, order: 2 },
-    { id: "4", name: "初期4行目", est: 5, passed: 2, order: 3 },
-    { id: "5", name: "初期5行目", est: 5, passed: 2, order: 4 },
-    { id: "6", name: "初期6行目", est: 5, passed: 2, order: 5 }
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "初期1行目", est: 5, passed: 2, order: 0 },
+    { id: 2, name: "初期2行目", est: 5, passed: 2, order: 1 },
+    { id: 3, name: "初期3行目", est: 10, passed: 2, order: 2 },
+    { id: 4, name: "初期4行目", est: 5, passed: 2, order: 3 },
+    { id: 5, name: "初期5行目", est: 5, passed: 2, order: 4 },
+    { id: 6, name: "初期6行目", est: 5, passed: 2, order: 5 }
   ]);
 
-  const estTotal = items.reduce((sum, i) => sum + i.est, 0);
-  const passedTotal = items.reduce((sum, i) => sum + i.passed, 0);
+  const estTotal = tasks.reduce((sum, i) => sum + i.est, 0);
+  const passedTotal = tasks.reduce((sum, i) => sum + i.passed, 0);
+
+  const addTasks = () => {
+    let lastId = 0;
+    let lastOrder = 0;
+    for (var i = 0, len = tasks.length; i < len; i++) {
+      // idの最大値
+      if (tasks[i].id > lastId) {
+        lastId = tasks[i].id;
+      };
+      // orderの最大値
+      if (tasks[i].order > lastOrder) {
+        lastOrder = tasks[i].order;
+      };
+    }
+
+    setTasks([...tasks, { id: lastId+1, name: "", est: 0, passed: 0, order: lastOrder+1 }])
+  };
 
   const onDrop = ({ removedIndex, addedIndex }) => {
-    const updater = (items) =>
-      arrayMoveImmutable(items, removedIndex, addedIndex).map((item, idx) => {
+    const updater = (tasks) =>
+      arrayMoveImmutable(tasks, removedIndex, addedIndex).map((item, idx) => {
         return { ...item, order: idx };
       });
-    setItems(updater);
+      setTasks(updater);
   };
+
+  document.addEventListener('dblclick', e => {
+    //if(dialogNode.current === null) return;
+    //if(dialogNode.current.contains(e.currentTarget)) return;
+    addTasks()
+  })
 
   return (
     <>
@@ -84,7 +107,7 @@ export const App = () => {
         <nav aria-label="secondary mailbox folders">
           <List>
             <Container onDrop={onDrop}>
-              {items.map(({ id, name, est, passed }) => (
+              {tasks.map(({ id, name, est, passed }) => (
                 <Draggable key={id}>
                   <ListItem style={{marginBottom: '9px', borderRadius: '3px', background: "#A5C9CA"}}>
                     <ListItemText primary={name}          style={{width: '72.8%'}} />
@@ -101,10 +124,10 @@ export const App = () => {
       </Box>
 
       <div>
-        <Box style={{height: 32, width: '60.3%', marginLeft: '9%',                        marginTop: 30, marginBottom: 8, float: "left", fontSize: 16, fontWeight: "bold", textAlign: "right", color: "#E7F6F2"}}>合計：</Box>
-        <Box style={{height: 32, width: '8.8%',  marginLeft: '2%',                        marginTop: 30, marginBottom: 8, float: "left", fontSize: 16, fontWeight: "bold", textAlign: "center", color: "#E7F6F2"}}>{estTotal} h</Box>
-        <Box style={{height: 32, width: '1%',    marginLeft: '0.6%', marginRight: '0.6%', marginTop: 30, marginBottom: 8, float: "left", fontSize: 16, fontWeight: "bold", textAlign: "center", color: "#E7F6F2"}}> / </Box>
-        <Box style={{height: 32, width: '8%',  marginRight: '9%',                       marginTop: 30, marginBottom: 8, float: "left", fontSize: 16, fontWeight: "bold", textAlign: "center", color: "#E7F6F2"}}>{passedTotal} h</Box>
+        <Box style={{height: 32, width: '4%',   marginLeft: '65.5%',                     marginTop: 30, marginBottom: 8, float: "left", fontSize: 16, fontWeight: "bold", textAlign: "right", color: "#E7F6F2"}}>合計：</Box>
+        <Box style={{height: 32, width: '8.8%', marginLeft: '2%',                        marginTop: 30, marginBottom: 8, float: "left", fontSize: 16, fontWeight: "bold", textAlign: "center", color: "#E7F6F2"}}>{estTotal} h</Box>
+        <Box style={{height: 32, width: '1%',   marginLeft: '0.6%', marginRight: '0.6%', marginTop: 30, marginBottom: 8, float: "left", fontSize: 16, fontWeight: "bold", textAlign: "center", color: "#E7F6F2"}}> / </Box>
+        <Box style={{height: 32, width: '8%',   marginRight: '9%',                       marginTop: 30, marginBottom: 8, float: "left", fontSize: 16, fontWeight: "bold", textAlign: "center", color: "#E7F6F2"}}>{passedTotal} h</Box>
       </div>
     </>
   );
