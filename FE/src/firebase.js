e39d2b788f8ee88fc1f89dd6417bab6a59c6ea4d
konan080211 <konanforbis@gmail.com>
2023-04-22 21:08:43 +0900

    fix: import方法の変更

--- FE/src/firebase.js
+++ FE/src/firebase.js
@@ -1,6 +1,7 @@
-import firebase from "firebase/app";
+import * as firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/functions";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
